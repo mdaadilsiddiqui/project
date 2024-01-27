c5f43504c9c272d78454c0e50b706e01949fce4e
mdaadilsiddiqui <mdsiddiquiaadil@gmail.com>
2024-01-27 17:45:33 +0530

    files

---
+++ app.js
@@ -0,0 +1,6 @@
+// ===== searchbox ======
+const search = document.querySelector('.searchbox');
+const icon = document.querySelector('.search-icon');
+search.onclick=function(){
+    search.classList.toggle('active')
+}
